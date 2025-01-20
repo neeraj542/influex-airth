e59318b94289507a1473b78d76e29433ee2f65c4
Neeraj Meena <51xneeraj@Neerajs-MacBook-Air.local>
2025-01-20 17:10:02 +0530

    vite updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  // Load environment variables based on mode
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      'process.env': env, // Make env variables accessible in code
+    },
+    build: {
+      outDir: 'dist',
+    },
+  })
+}
