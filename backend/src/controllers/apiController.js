f80954862d72228dbf8bea450ec319981eae1a1b
Neeraj Meena <114648043+neeraj542@users.noreply.github.com>
2025-01-30 01:53:07 +0530

    Update apiController.js

--- backend/src/controllers/apiController.js
+++ backend/src/controllers/apiController.js
@@ -19,7 +19,7 @@ exports.exchangeToken = async (req, res) => {
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
     grant_type: 'authorization_code',
-    redirect_uri: process.env.REDIRECT_URI,
+    redirect_uri: 'https://item-list-manager-neeraj542.vercel.app/auth/redirect',
     code: authCode,
   };
 
