f4040e013a9d8b24d2b28f1c1fb43a66683654ef
Neeraj Meena <114648043+neeraj542@users.noreply.github.com>
2025-01-29 15:26:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
-  // Load environment variables based on mode
+  // Load environment variables based on the mode (development/production)
   const env = loadEnv(mode, process.cwd())
 
   return defineConfig({
     plugins: [react()],
-    define: {
-      'process.env': env, // Make env variables accessible in code
-    },
     build: {
       outDir: 'dist',
     },
