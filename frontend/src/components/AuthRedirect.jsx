952839fad8440fc9c33c6a9905f997e2c1d99cce
Neeraj Meena <114648043+neeraj542@users.noreply.github.com>
2025-01-30 02:56:36 +0530

    Create AuthRedirect.jsx

---
+++ frontend/src/components/AuthRedirect.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './layout/Header';
+import FAQForm from '../pages/FAQForm';
+import Footer from './layout/Footer';
+
+const AuthRedirect = () => (
+  <>
+    <Header />
+    <FAQForm />
+    <Footer />
+  </>
+);
+
+export default AuthRedirect;
