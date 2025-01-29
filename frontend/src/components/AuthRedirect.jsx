a5cdfcc7c54ef7c5f1fd157bf20f65779ad592cb
Neeraj Meena <114648043+neeraj542@users.noreply.github.com>
2025-01-30 02:57:41 +0530

    Update AuthRedirect.jsx

--- frontend/src/components/AuthRedirect.jsx
+++ frontend/src/components/AuthRedirect.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from './layout/Header';
 import FAQForm from '../pages/FAQForm';
 import Footer from './layout/Footer';
 
 const AuthRedirect = () => (
   <>
-    <Header />
     <FAQForm />
     <Footer />
   </>
