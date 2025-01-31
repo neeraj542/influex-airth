232bd72d2cd068f59b2b49a4b21226079910cfec
Neeraj Meena <114648043+neeraj542@users.noreply.github.com>
2025-01-31 16:01:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 export default ({ mode }) => {
   // Load environment variables based on the mode (development/production)
@@ -9,6 +10,11 @@ export default ({ mode }) => {
     plugins: [react()],
     build: {
       outDir: 'dist',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
   })
 }
